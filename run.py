96c3784524aa8a80df9e48f4578f6eda918b05f6
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2024-12-05 18:01:26 +0000

    adjust validator to check input is all integers

--- run.py
+++ run.py
@@ -25,7 +25,22 @@ def get_sales_data():
     print("Ex: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all strs to ints
+    Raises value error if strs cannot be converted or if there are not exactly 6 values
+    """
+    try:
+        [int(value) for value in values]
+        if len(values)!=6:
+            raise ValueError(f"Exactly 6 values required; you provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}. Please try again.")
+        
+
 
 get_sales_data()
 
