98aee44ab1d901ea691f22705dc17b282164c829
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-01-05 12:49:36 +0000

    Update run.py

--- run.py
+++ run.py
@@ -102,7 +102,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(surplus_data)
+    update_surplus_worksheet(new_surplus_data)
     print(new_surplus_data)
 
 
